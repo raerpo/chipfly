f8b1e6b6916edd23ce7435e6c6ccdad187c5be9f
Rafael Poveda <raerpo@gmail.com>
2018-04-06 14:55:47 -0300

    add downshift for input autocomplete

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-export default () => (
+import MainInput from '../components/mainInput';
+
+const Index = ({ cities }) => (
   <div>
-    Hello world!
+    <h1>Where do you want to go?</h1>
+    <MainInput
+      items={cities}
+      onChange={selectedItem => console.log(selectedItem)}
+    />
   </div>
 );
+
+Index.getInitialProps = () => {
+  const cities = [
+    'Bogota',
+    'Valdivia',
+    'Buenos aires'
+  ];
+  return {
+    cities
+  };
+}
+
+export default Index;
