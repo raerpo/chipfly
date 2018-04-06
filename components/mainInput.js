0cd1b373872a219e890aa2bcfb5918bd4ab1fa24
Rafael Poveda <raerpo@gmail.com>
2018-04-06 14:56:27 -0300

    add main input component

---
+++ components/mainInput.js
@@ -0,0 +1,46 @@
+import Downshift from 'downshift';
+
+function InputAutocomplete({items, onChange}) {
+  return (
+    <Downshift
+      onChange={onChange}
+      render={({
+        getInputProps,
+        getItemProps,
+        isOpen,
+        inputValue,
+        selectedItem,
+        highlightedIndex,
+      }) => (
+        <div>
+          <input {...getInputProps({placeholder: 'Favorite fruit ?'})} />
+          {isOpen ? (
+            <div style={{border: '1px solid #ccc'}}>
+              {items
+                .filter(
+                  i =>
+                    !inputValue ||
+                    i.toLowerCase().includes(inputValue.toLowerCase()),
+                )
+                .map((item, index) => (
+                  <div
+                    {...getItemProps({item})}
+                    key={item}
+                    style={{
+                      backgroundColor:
+                        highlightedIndex === index ? 'gray' : 'white',
+                      fontWeight: selectedItem === item ? 'bold' : 'normal',
+                    }}
+                  >
+                    {item}
+                  </div>
+                ))}
+            </div>
+          ) : null}
+        </div>
+      )}
+    />
+  )
+}
+
+export default InputAutocomplete;
